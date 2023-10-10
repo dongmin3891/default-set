6909a791d7a8fc0ae92f09e4d556b287e23cc31f
dongmin3891 <rlaehdals753@naver.com>
2023-10-10 18:46:51 +0900

    feat: first-commit

---
+++ frontend/src/app/test/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { Button } from '@/components/Button'
+import Image from 'next/image'
+
+export default function Test() {
+  return (
+    <>
+      <Button label='테스트' backgroundColor='red'/>
+      <h1>테스트</h1>
+    </>
+  )
+}
